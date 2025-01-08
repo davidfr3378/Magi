bae9a7bf86552f44b49e0320ddaa08f2e53a7b65
davidfr3378 <157181930+davidfr3378@users.noreply.github.com>
2025-01-08 09:05:19 -0500

    Update spells.java
    removed the blasted enums

--- src/magi/spells.java
+++ src/magi/spells.java
@@ -9,22 +9,7 @@ public class spells {
     private int offence, defence;
     private String name;
     private spell_subclass aSpell_subclass;
-    
-    private enum spell_subclass {
-    ELEMENTAL,
-    ENERGY,
-    SOUL,
-    SUMMONER,
-    SECRET
-  }
-    
-    enum elemental {
 
-    }
-    
-    
-    
-    
     public spells(String name, int offence, spell_subclass aSpell_subclass){
         
     }
